9158a37b87bb4ad63b0d15355a086842f0b47b31
Naveen <naveen.varadharaj@vanderbilt.edu>
2018-12-12 00:58:13 -0600

    update package.json

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { Component } from "react";
 import $ from "jquery";
-import { DateRangePicker } from 'react-date-range';
-import { addDays } from 'date-fns';
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { addDays } from "date-fns";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 import "./App.css";
 
 const genres = [
-  {label: "Sports", value: "sports"},
-  {label: "Music", value: "music"},
-  {label: "Arts & Theatre", value: "arts %26 theatre"},
+  { label: "Sports", value: "sports" },
+  { label: "Music", value: "music" },
+  { label: "Arts & Theatre", value: "arts %26 theatre" }
 ];
 
-const monthMap =
-{
-  'Jan' : '01',
-  'Feb' : '02',
-  'Mar' : '03',
-  'Apr' : '04',
-  'May' : '05',
-  'Jun' : '06',
-  'Jul' : '07',
-  'Aug' : '08',
-  'Sep' : '09',
-  'Oct' : '10',
-  'Nov' : '11',
-  'Dec' : '12'
+const monthMap = {
+  Jan: "01",
+  Feb: "02",
+  Mar: "03",
+  Apr: "04",
+  May: "05",
+  Jun: "06",
+  Jul: "07",
+  Aug: "08",
+  Sep: "09",
+  Oct: "10",
+  Nov: "11",
+  Dec: "12"
 };
 
 class App extends Component {
@@ -33,22 +32,24 @@ class App extends Component {
     super(props);
     this.state = {
       genrePicked: "",
-      rangeStart: '2018-12-12T00:00:01Z',
-      rangeEnd: '2018-12-19T23:59:59Z',
-      dateRange: { // represents the potential dates
+      rangeStart: "2018-12-12T00:00:01Z",
+      rangeEnd: "2018-12-19T23:59:59Z",
+      dateRange: {
+        // represents the potential dates
         selection: {
           startDate: new Date(),
           endDate: null,
-          key: 'selection',
-        },
+          key: "selection"
+        }
       },
-      dateRangePicker: { // represents the dates selected
+      dateRangePicker: {
+        // represents the dates selected
         selection: {
           startDate: new Date(),
           endDate: addDays(new Date(), 7),
-          key: 'selection',
-        },
-      },
+          key: "selection"
+        }
+      }
     };
   }
 
@@ -84,17 +85,24 @@ class App extends Component {
     this.setState({
       [which]: {
         ...this.state[which],
-        ...payload,
+        ...payload
       },
-      rangeStart: payload.selection.startDate.toString().substring(11, 15)
-        + '-' + monthMap[payload.selection.startDate.toString().substring(4, 7)] + '-'
-        + payload.selection.startDate.toString().substring(8, 10) + 'T00:00:01Z',
-      rangeEnd: payload.selection.endDate.toString().substring(11, 15)
-        + '-' + monthMap[payload.selection.endDate.toString().substring(4, 7)] + '-'
-        + payload.selection.endDate.toString().substring(8, 10) + 'T23:59:59Z',
+      rangeStart:
+        payload.selection.startDate.toString().substring(11, 15) +
+        "-" +
+        monthMap[payload.selection.startDate.toString().substring(4, 7)] +
+        "-" +
+        payload.selection.startDate.toString().substring(8, 10) +
+        "T00:00:01Z",
+      rangeEnd:
+        payload.selection.endDate.toString().substring(11, 15) +
+        "-" +
+        monthMap[payload.selection.endDate.toString().substring(4, 7)] +
+        "-" +
+        payload.selection.endDate.toString().substring(8, 10) +
+        "T23:59:59Z"
     });
   }
-
 
   render() {
     var { searching, eventResult } = this.state;
@@ -107,10 +115,10 @@ class App extends Component {
       <div className="App">
         <div className="dateRange">
           <DateRangePicker
-            onChange={this.handleRangeChange.bind(this, 'dateRangePicker')}
+            onChange={this.handleRangeChange.bind(this, "dateRangePicker")}
             showSelectionPreview={true}
             moveRangeOnFirstSelection={false}
-            className={'PreviewArea'}
+            className={"PreviewArea"}
             months={2}
             ranges={[this.state.dateRangePicker.selection]}
             direction="horizontal"
